c04081de951db77e23e4084fe03a48167d16505e
OdiseU <secret36565@gmail.com>
2022-12-27 00:02:42 +0900

    feature: Env 서비스 모듈 작성

--- projects/server/services/config.service.ts
+++ projects/server/services/config.service.ts
@@ -1,12 +1,6 @@
-import dotenv from 'dotenv';
-import path from 'path';
-
-const ENV_PATH = path.join(path.resolve(), `./.env/.env.${process.env.NODE_ENV}`)
-
-dotenv.config({ path: ENV_PATH });
+import Env from './env.service';
 
 interface IConfig {
-    readonly ENV: string;
     readonly SERVER_HOST: string;
     readonly SERVER_PORT: string;
     readonly MONGODB_URL: string;
@@ -14,23 +8,12 @@ interface IConfig {
     readonly GSI_CLIENT_ID: string;
 }
 
-const getEnv = (key: string):string => {
-    const value = process.env[key];
-    
-    if(value === undefined) {
-        throw new Error(`can not get env '${key}'`);
-    }
-    
-    return value;
-}
-
 const Config: IConfig = {
-    "ENV": getEnv('NODE_ENV'),
-    "SERVER_HOST": getEnv('SERVER_HOST'),
-    "SERVER_PORT": getEnv('SERVER_PORT'),
-    "MONGODB_URL": getEnv('MONGODB_URL'),
-    "MONGODB_NAME": getEnv('MONGODB_NAME'),
-    "GSI_CLIENT_ID": getEnv('GSI_CLIENT_ID'),
+    "SERVER_HOST": Env.get('SERVER_HOST'),
+    "SERVER_PORT": Env.get('SERVER_PORT'),
+    "MONGODB_URL": Env.get('MONGODB_URL'),
+    "MONGODB_NAME": Env.get('MONGODB_NAME'),
+    "GSI_CLIENT_ID": Env.get('GSI_CLIENT_ID'),
 };
 
 export default Config;
