fb1088e0b327d5954ab1bf19250a815dd592d313
OdiseU <secret36565@gmail.com>
2023-08-02 23:02:04 +0900

    modify prettier code

--- projects/server/app.ts
+++ projects/server/app.ts
@@ -1,19 +1,19 @@
-import express, { urlencoded, json } from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import Config from './services/config.service';
-import Env from './services/env.service';
-import Logger from './services/logger.service';
-import Morgan from './services/morgan.service';
+import express, { urlencoded, json } from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import Config from "./services/config.service";
+import Env from "./services/env.service";
+import Logger from "./services/logger.service";
+import Morgan from "./services/morgan.service";
 
 const app = express();
 
 // for link react client app
-const clientPath = path.join(path.resolve(), '../front/build');
+const clientPath = path.join(path.resolve(), "../front/build");
 app.use(express.static(clientPath));
 
 // for read request.body
-app.use(urlencoded({extended: true}));
+app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(json());
 
@@ -21,7 +21,7 @@ app.use(json());
 app.use(Morgan);
 
 // start server
-app.listen(Config.SERVER_PORT, ()=> {
+app.listen(Config.SERVER_PORT, () => {
     Logger.debug(`> Server on ${Config.SERVER_HOST}:${Config.SERVER_PORT}`);
     Logger.debug(`> ${Env.mode.toUpperCase()} MODE`);
 });
--- projects/server/services/morgan.service.ts
+++ projects/server/services/morgan.service.ts
@@ -1,22 +1,25 @@
-import { IncomingMessage, ServerResponse } from 'http';
-import morgan from 'morgan';
-import Env from './env.service';
-import Logger from './logger.service';
+import { IncomingMessage, ServerResponse } from "http";
+import morgan from "morgan";
+import Env from "./env.service";
+import Logger from "./logger.service";
 
 const format = () => {
-    if(Env.isProd()) return ':method :url :status :response-time ms';
-    return ':method :url :status :response-time ms';
-}
+    if (Env.isProd()) return ":method :url :status :response-time ms";
+    return ":method :url :status :response-time ms";
+};
 
 const stream = {
     write: (message: string) => {
         Logger.http(message);
-    }
-}
+    },
+};
 
-const skip = (request: IncomingMessage, response: ServerResponse<IncomingMessage>): boolean => {
+const skip = (
+    request: IncomingMessage,
+    response: ServerResponse<IncomingMessage>
+): boolean => {
     return false;
-}
+};
 
 const Morgan = morgan(format(), { stream, skip });
 
